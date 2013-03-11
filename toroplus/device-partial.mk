e595b75d53664c4a87b699b8bb38d15006161a26
something15525 <something15525@gmail.com>
2013-03-11 11:26:59 -0700

    toroplus: Update binaries to 4.2.2

--- toroplus/device-partial.mk
+++ toroplus/device-partial.mk
@@ -19,22 +19,13 @@ PRODUCT_PACKAGES := \
     SDM \
     SecPhone \
     SyncMLSvc \
+    ducati-m3 \
     fRom \
+    gps.omap4 \
+    lib_gsd4t \
+    libsec-ril_lte \
     libsecril-client \
     libsyncml_core \
     libsyncml_port \
-    lib_gsd4t \
-    libsec-ril_lte \
-    libdrmwvmplugin \
     libwvm \
-    fw_bcmdhd \
-    fw_bcmdhd_apsta \
-    fw_bcmdhd_p2p \
-    libfrsdk \
-    libgcomm_jni \
-    libspeexwrapper \
-    mms144_ts_rev31 \
-    mms144_ts_rev32 \
-    smc_normal_world_android_cfg \
-    smc_pa_wvdrm \
-    wmdsi
+    sirfgps
